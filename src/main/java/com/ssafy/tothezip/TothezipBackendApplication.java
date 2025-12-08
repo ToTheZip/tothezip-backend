a118ffdebeef2519e1cba70971e5f18c87810fac
sseooh <sseooh@gmail.com>
2025-12-08 17:51:46 +0900

    init project

---
+++ src/main/java/com/ssafy/tothezip/TothezipBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tothezip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TothezipBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TothezipBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssafy/tothezip/TothezipBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tothezip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TothezipBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
